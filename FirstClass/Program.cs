2fcace52c3772846774f53ceacc62b15cc109037
Feaxmutch <feaxmutch@gmail.com>
2024-10-21 14:41:59 +0300

    Created player class

--- FirstClass/Program.cs
+++ FirstClass/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+            Player player = new("BaTpyshKa", 100);
+            player.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+
+    public class Player
+    {
+        public Player(string name, int health)
+        {
+            Name = name;
+            Health = health;
+        }
+
+        public string Name { get; }
+
+        public int Health { get; private set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Имя: {Name}");
+            Console.WriteLine($"Здоровье: {Health}");
         }
     }
 }
